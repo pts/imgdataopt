f95a0aaedbb3c11054b82d221602542e0c9bbc2e
Peter Szabo <pts@fazekas.hu>
2023-02-20 20:02:36 +0100

    added compilation shell scripts for macOS: c-darwinc32.sh and c-darwinc64.sh

---
+++ c-darwinc32.sh
@@ -0,0 +1,14 @@
+#! /bin/sh --
+set -ex
+
+if type i386-apple-darwin14-gcc >/dev/null 2>&1; then
+  PREFIX=  # https://github.com/pts/pts-osxcross
+else
+  PREFIX="docker run -v --rm -it $PWD:/workdir multiarch/crossbuild /usr/osxcross/bin/"
+fi
+
+
+${PREFIX}i386-apple-darwin14-gcc -mmacosx-version-min=10.5 -Wl,-dead_strip -ffunction-sections -fdata-sections -lSystem -lcrt1.10.5.o -nostdlib -Izlib_src -DNO_VIZ -DNO_COMBINE64 -ansi -pedantic -O2 -W -Wall -Wextra -Werror=implicit-function-declaration -Wno-variadic-macros -o imgdataopt.darwinc32 imgdataopt.c zlib_src/zall.c
+${PREFIX}i386-apple-darwin14-strip imgdataopt.darwinc32
+
+: "$0" OK.
