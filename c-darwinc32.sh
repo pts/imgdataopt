384aec64661c3b5e667c4d002d7bfe5b1241fc2c
Peter Szabo <pts@fazekas.hu>
2023-03-07 15:48:45 +0100

    added mini targets imgdataopt.darwinc32mini and imgdataopt.darwinc64mini to be used with pdfsizeopt

--- c-darwinc32.sh
+++ c-darwinc32.sh
@@ -10,5 +10,7 @@ fi
 
 ${PREFIX}i386-apple-darwin14-gcc -mmacosx-version-min=10.5 -Wl,-dead_strip -ffunction-sections -fdata-sections -lSystem -lcrt1.10.5.o -nostdlib -Izlib_src -DNO_VIZ -DNO_COMBINE64 -ansi -pedantic -O2 -W -Wall -Wextra -Werror=implicit-function-declaration -Wno-variadic-macros -o imgdataopt.darwinc32 imgdataopt.c zlib_src/zall.c
 ${PREFIX}i386-apple-darwin14-strip imgdataopt.darwinc32
+${PREFIX}i386-apple-darwin14-gcc -mmacosx-version-min=10.5 -Wl,-dead_strip -ffunction-sections -fdata-sections -lSystem -lcrt1.10.5.o -nostdlib -Izlib_src -DNO_VIZ -DNO_COMBINE64 -DNO_PMTIFF -DNO_PNM -DNO_REGTEST -ansi -pedantic -O2 -W -Wall -Wextra -Werror=implicit-function-declaration -Wno-variadic-macros -o imgdataopt.darwinc32mini imgdataopt.c zlib_src/zall.c
+${PREFIX}i386-apple-darwin14-strip imgdataopt.darwinc32mini
 
 : "$0" OK.
--- c-darwinc64.sh
+++ c-darwinc64.sh
@@ -10,5 +10,7 @@ fi
 
 ${PREFIX}x86_64-apple-darwin14-gcc -mmacosx-version-min=10.5 -Wl,-dead_strip -ffunction-sections -fdata-sections -lSystem -lcrt1.10.5.o -nostdlib -Izlib_src -DNO_VIZ -DNO_COMBINE64 -ansi -pedantic -O2 -W -Wall -Wextra -Werror=implicit-function-declaration -Wno-variadic-macros -o imgdataopt.darwinc64 imgdataopt.c zlib_src/zall.c
 ${PREFIX}x86_64-apple-darwin14-strip imgdataopt.darwinc64
+${PREFIX}x86_64-apple-darwin14-gcc -mmacosx-version-min=10.5 -Wl,-dead_strip -ffunction-sections -fdata-sections -lSystem -lcrt1.10.5.o -nostdlib -Izlib_src -DNO_VIZ -DNO_COMBINE64 -DNO_PMTIFF -DNO_PNM -DNO_REGTEST -ansi -pedantic -O2 -W -Wall -Wextra -Werror=implicit-function-declaration -Wno-variadic-macros -o imgdataopt.darwinc64mini imgdataopt.c zlib_src/zall.c
+${PREFIX}x86_64-apple-darwin14-strip imgdataopt.darwinc64mini
 
 : "$0" OK.
